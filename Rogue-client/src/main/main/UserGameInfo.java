2ab4ee8d978a26c6cb84dc78182340fc8c8a3b9a
olya.zelenkova <olya.zelenkova94@mail.ru>
2014-10-27 12:36:15 -0300

    Добавила поля

--- Rogue-client/src/main/main/UserGameInfo.java
+++ Rogue-client/src/main/main/UserGameInfo.java
@@ -5,6 +5,18 @@ import sun.reflect.generics.reflectiveObjects.NotImplementedException;
 public class UserGameInfo {
 	
 	// TODO Fields and getters and setters
+	private String userName; 
+	private boolean finished;
+	private int level; 
+	private int hp; 
+	private int mana;
+	private int satiety; 
+	private int attack;
+	private int defense; 
+	private int vision;
+	private double hpReg; 
+	private double manaReg; 
+	private int experience;
 	
 	public String toJson() {
 		// TODO Serialization
